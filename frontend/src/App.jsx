774edc123538de71ab2d7d548021b0d770c1480f
Susheel Thapa <83917129+SusheelThapa@users.noreply.github.com>
2023-12-22 16:00:32 +0545

    Implemented typing features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,39 @@
-import { useState, useEffect } from "react";
+import { useRef, useEffect } from "react";
+import Typed from "typed.js";
 
 const App = () => {
-  const texts = ["Languages", "Reviews", "Quizzes"];
-
-  const [text, setText] = useState(texts[0]);
+  const typed_text = useRef(null);
 
   useEffect(() => {
-    let index = 0;
+    const typed = new Typed(typed_text.current, {
+      strings: ["Languages", "Reviews", "Quizzes"],
+      startDelay: 300,
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 200,
+      loop: true,
+      showCursor: true,
+      cursorChar: "🔥",
+    });
 
-    const timer = setInterval(() => {
-      index = (index + 1) % texts.length;
-      setText(texts[index]);
-    }, 2000);
-
-    return () => clearInterval(timer);
-  });
+    return () => {
+      typed.destroy();
+    };
+  }, []);
 
   return (
     <div className="bg-black w-screen   h-screen text-white flex flex-col items-center justify-center">
       <h1 className="text-8xl m-10">
         <span className="text-white">dev</span>
-        <span className="text-green-600	bold underline">Quine</span>
+        <span className="text-green-400	bold underline">Quine</span>
       </h1>
       <div className="text-4xl font-family-poppins">
         Transforming Coding with{" "}
-        <span className="text-orange-500 underline bold">{text}</span>.
+        <span
+          className="text-orange-500 underline bold"
+          ref={typed_text}
+        ></span>
+        .
       </div>
     </div>
   );
