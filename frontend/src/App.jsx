49b1bc1987615aaa08ba0183c44c4fd54d249922
Susheel Thapa <83917129+SusheelThapa@users.noreply.github.com>
2023-12-22 15:43:22 +0545

    Created landing page for devQuine

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,33 @@
+import { useState, useEffect } from "react";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">devQuine</h1>;
+  const texts = ["Languages", "Reviews", "Quizzes"];
+
+  const [text, setText] = useState(texts[0]);
+
+  useEffect(() => {
+    let index = 0;
+
+    const timer = setInterval(() => {
+      index = (index + 1) % texts.length;
+      setText(texts[index]);
+    }, 2000);
+
+    return () => clearInterval(timer);
+  });
+
+  return (
+    <div className="bg-black w-screen   h-screen text-white flex flex-col items-center justify-center">
+      <h1 className="text-8xl m-10">
+        <span className="text-white">dev</span>
+        <span className="text-green-600	bold underline">Quine</span>
+      </h1>
+      <div className="text-4xl font-family-poppins">
+        Transforming Coding with{" "}
+        <span className="text-orange-500 underline bold">{text}</span>.
+      </div>
+    </div>
+  );
 };
 
 export default App;
