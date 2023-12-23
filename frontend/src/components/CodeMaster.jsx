96f5ae27ba7ca94a1e9fb88b4299492dc2efb685
Susheel Thapa <83917129+SusheelThapa@users.noreply.github.com>
2023-12-23 18:21:26 +0545

    Implement Carousel in FlashCards

--- frontend/src/components/CodeMaster.jsx
+++ frontend/src/components/CodeMaster.jsx
@@ -1,17 +1,19 @@
 import CodeMasterForm from "./CodeMasterForm";
 import FlashCardCarousel from "./FlashCardCarousel";
 
-import flashCardsDetail from "../json/flashcard.json";
+import { data } from "../json/flashcard.json";
 const CodeMaster = () => {
   const handleSubmit = (topic, numQuestions) => {
     console.log(`Form Submitted with ${numQuestions} question on ${topic}`);
 
     // API Call
   };
+
+  console.log(data);
   return (
     <div className="flex justify-around items-center  m-10 p-10">
       <CodeMasterForm handleFormSubmit={handleSubmit} />
-      <FlashCardCarousel flashCardsDetail={flashCardsDetail} />
+      <FlashCardCarousel flashCardData={data} />
     </div>
   );
 };
