0d9e2d14b3263e5fdcd11316bdab26d76010fbee
Susheel Thapa <83917129+SusheelThapa@users.noreply.github.com>
2023-12-23 21:28:54 +0545

    Create texteditor with option of change language and theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,18 @@ import "./index.css";
 
 import "./fonts/Poppins-Regular.ttf";
 
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/mode-c_cpp";
+
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/theme-tomorrow";
+import "ace-builds/src-noconflict/theme-kuroir";
+import "ace-builds/src-noconflict/theme-twilight";
+
+import "ace-builds/src-noconflict/ext-language_tools"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
