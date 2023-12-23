33370f8240359f94107ec4d77cb43fe3b5492196
Susheel Thapa <83917129+SusheelThapa@users.noreply.github.com>
2023-12-23 18:21:26 +0545

    Implement Carousel in FlashCards

--- frontend/src/components/FlashCardCarousel.jsx
+++ frontend/src/components/FlashCardCarousel.jsx
@@ -1,13 +1,45 @@
+import { useState } from "react";
 import FlashCard from "./common/FlashCard";
 
-const FlashCardCarousel = ({ flashCardsDetail }) => {
+import PropTypes from "prop-types";
+
+import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
+
+const FlashCardCarousel = ({ flashCardData }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPrevious = () => {
+    setCurrentIndex(currentIndex - 1);
+  };
+
+  const goToNext = () => {
+    setCurrentIndex(currentIndex + 1);
+  };
+
+  console.log(`Current index ${currentIndex}`);
   return (
-    <>
-      {flashCardsDetail.map(({ question, answer }) => {
-        <FlashCard className="w-2/3" question={question} answer={answer} />;
-      })}
-    </>
+    <div className="carousel-container flex justify-center items-center">
+      {currentIndex > 0 && (
+        <button onClick={goToPrevious} className="left-arrow text-2xl text-red-500">
+          <FaArrowLeft />
+        </button>
+      )}
+      <FlashCard
+        question={flashCardData[currentIndex].question}
+        answer={flashCardData[currentIndex].answer}
+      />
+
+      {currentIndex < flashCardData.length - 1 && (
+        <button onClick={goToNext} className="right-arrow text-2xl text-red-500">
+          <FaArrowRight />
+        </button>
+      )}
+    </div>
   );
 };
 
+FlashCardCarousel.propTypes = {
+  flashCardData: PropTypes.array,
+};
 export default FlashCardCarousel;
