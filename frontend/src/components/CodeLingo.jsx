0983ec1850af8ddbfe83034a1eac1ca6dcb4878d
Susheel Thapa <83917129+SusheelThapa@users.noreply.github.com>
2023-12-24 07:27:04 +0545

    Minor Customization in editor section

--- frontend/src/components/CodeLingo.jsx
+++ frontend/src/components/CodeLingo.jsx
@@ -3,8 +3,8 @@ import Editor from "./common/Editor";
 const CodeLingo = () => {
   return (
     <div className="flex justify-between bg-gray-50 items-center px-5  mx-64 m-16 rounded-3xl">
-      <Editor className="w-1/2" />
-      <Editor className="w-1/2" />
+      <Editor className="w-1/2" autofocus={true}  />
+      <Editor className="w-1/2" read_only={true} />
     </div>
   );
 };
